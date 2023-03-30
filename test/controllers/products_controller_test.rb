d0f74f1a66b7555161e56d1781696cf2e5dd0a0c
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-30 15:09:01 -0400

    added patch route

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -16,4 +16,10 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal ["id", "name", "price", "image_url", "description", "created_at", "updated_at"], data.keys
   end
+
+  test “create” do
+    assert_difference “Product.count”, 1 do
+      post “/products.json”, params: { name: “name”, price: 1, image_url: “image.jpg”, description: “description" }
+    end
+    end
 end
