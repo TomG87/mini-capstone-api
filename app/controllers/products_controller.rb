b2b15e5d444d77dde799c111975cc0acb67190da
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-30 14:33:54 -0400

    added post route to add new id's

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,5 @@
 class ProductsController < ApplicationController
 
-  # def sole
-  #   @product = Product.first
-  #   render template: "products/show"
-  # end
-
-  # def index
-  #   @products = Product.all
-  #   render template: "products/index"
-  # end
 
   def index
     @products = Product.all
@@ -17,6 +8,17 @@ class ProductsController < ApplicationController
 
   def show    
     @product =  Product.find_by(id: params[:id])
+    render :show
+  end
+  
+  def create
+    @product = Product.new(
+      name: params[:name], 
+      price: params[:price], 
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    @product.save
     render :show
   end
 end
