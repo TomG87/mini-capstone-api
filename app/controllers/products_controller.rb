f47d0a99df1775d3bf68b89cbe5fd9328517eb11
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-31 12:23:23 -0400

    added defaults for destory route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,10 +24,10 @@ class ProductsController < ApplicationController
 
   def update
     @product = Product.find_by(id: params[:id])
-    @product.name = params[:name]
-    @product.price = params[:price]
-    @product.image_url = params[:image_url]
-    @product.description = params[:description]
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
     @product.save
     render :show
   end
@@ -36,6 +36,5 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     @product.destroy
     render json: {message: "Product has been successfully removed"}
-  end
-  
+  end 
 end
