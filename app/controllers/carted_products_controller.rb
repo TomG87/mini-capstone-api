e1e64624b5088657acc0a51ccc29bebdd7d79e75
Tom Gagliano <tgaglianoll@gmail.com>
2023-04-20 14:57:57 -0400

    created products function

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,4 +1,23 @@
 class CartedProductsController < ApplicationController
+
   def create
-    
+    @carted_product = CartedProduct.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quanatity],
+      status: "carted"
+    )
+    @carted_product.save
+    render :show
+  end
+
+  def index
+    while current_user.id == true 
+        if CartedProducts.status == "carted"
+          render :index
+        else
+          render json: {message: "only carted items will show"}
+        end
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
 
   validates :name, length: {minimum:3}
   validates :price, presence: true
